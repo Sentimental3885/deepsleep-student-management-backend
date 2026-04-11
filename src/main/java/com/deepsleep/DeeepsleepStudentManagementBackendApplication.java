e1479584d614603fdf73c5a70989e9fcfc714869
Sentimental <CarolineFloyd3885@gmail.com>
2026-04-11 08:52:09 +0800

    chore(初始化): DeepSleep学生管理系统项目初始化

---
+++ src/main/java/com/deepsleep/DeeepsleepStudentManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.deepsleep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeeepsleepStudentManagementBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeeepsleepStudentManagementBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/deepsleep/DeeepsleepStudentManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepsleep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeeepsleepStudentManagementBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
